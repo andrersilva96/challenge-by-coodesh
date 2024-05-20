b5a841f5c7fba635c7e2b2b47fac0a5c5651a8ca
Andre Rodrigues <andre.rsilva96@hotmail.com>
2024-05-20 15:07:08 -0300

    Save foods at database

---
+++ database/migrations/2024_05_20_171613_create_foods_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('foods', function (Blueprint $table) {
+            $table->id();
+            $table->string('code');
+            $table->string('status')->nullable();
+            $table->string('url')->nullable();
+            $table->string('creator')->nullable();
+            $table->string('product_name')->nullable();
+            $table->string('quantity')->nullable();
+            $table->string('brands')->nullable();
+            $table->text('categories')->nullable();
+            $table->string('labels')->nullable();
+            $table->string('cities')->nullable();
+            $table->string('purchase_places')->nullable();
+            $table->string('stores')->nullable();
+            $table->text('ingredients_text')->nullable();
+            $table->string('traces')->nullable();
+            $table->string('serving_size')->nullable();
+            $table->string('serving_quantity')->nullable();
+            $table->string('nutriscore_score')->nullable();
+            $table->string('nutriscore_grade')->nullable();
+            $table->string('main_category')->nullable();
+            $table->string('image_url')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('food');
+    }
+};
